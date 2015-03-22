5e9b495feb5e6d495578f665889fc48c5f6d7285
izurururu911 <izuru.okumura@gmail.com>
2015-03-23 00:57:42 +0900

    Initial Commit

---
+++ oasobi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  oasobi
+//
+//  Created by 奥村 維敦 on 2015/03/23.
+//  Copyright (c) 2015年 奥村 維敦. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
